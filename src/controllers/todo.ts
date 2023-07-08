daf1f0c10f5d591ad6cf49073a7c95edc98df052
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-08 09:07:08 +0300

    add getTodo controller

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -11,3 +11,22 @@ export const getTodos = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ success: false });
   }
 };
+
+export const getTodo = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = Number(req.params.id);
+    const todo = await prisma.todo.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!todo) {
+      res.status(404).json({ success: false });
+      return;
+    }
+
+    res.status(200).json({ success: true, data: todo });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+};
