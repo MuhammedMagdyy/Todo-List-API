92162d77874e0ba1d60e992f1988cc4ec510f4b5
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-08 09:12:37 +0300

    add deleteTodo router

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getTodos, getTodo, createTodo, updateTodo } from '../controllers/todo';
+import { getTodos, getTodo, createTodo, updateTodo, deleteTodo } from '../controllers/todo';
 
 const router: Router = Router();
 
@@ -7,5 +7,6 @@ router.get('/todos', getTodos);
 router.get('/todos/:id', getTodo);
 router.post('/todos', createTodo);
 router.patch('/todos/:id', updateTodo);
+router.delete('/todos/:id', deleteTodo);
 
 export default router;
