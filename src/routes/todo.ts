dfc2534f58af31548df428f852ebd205a83ce1a6
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-08 09:11:11 +0300

    add updateTodo router

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getTodos, getTodo, createTodo } from '../controllers/todo';
+import { getTodos, getTodo, createTodo, updateTodo } from '../controllers/todo';
 
 const router: Router = Router();
 
 router.get('/todos', getTodos);
 router.get('/todos/:id', getTodo);
 router.post('/todos', createTodo);
+router.patch('/todos/:id', updateTodo);
 
 export default router;
