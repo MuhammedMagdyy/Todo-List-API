22dc1108ff67fc4f087b8d6b909dd11719d0460a
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-08 09:05:17 +0300

    add getTodos controller

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -2,3 +2,12 @@ import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const prisma: PrismaClient = new PrismaClient();
+
+export const getTodos = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const todos = await prisma.todo.findMany();
+    res.status(200).json({ success: true, data: todos });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+};
