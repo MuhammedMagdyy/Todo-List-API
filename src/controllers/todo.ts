01268051df29c54f4654ad8ede7b095c46202931
MuhammedMagdyy <mohamedmody04@gmail.comm>
2023-07-08 09:10:32 +0300

    add updateTodo controller

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -57,3 +57,31 @@ export const createTodo = async (
     res.status(500).json({ success: false });
   }
 };
+
+export const updateTodo = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const id = Number(req.params.id);
+    const { title, content, completed } = req.body;
+    const todo = await prisma.todo.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title,
+        content,
+        completed,
+      },
+    });
+    if (!todo) {
+      res.status(404).json({ success: false });
+      return;
+    }
+
+    res.status(200).json({ success: true, data: todo });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+};
