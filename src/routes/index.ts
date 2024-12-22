7cdc16bcbcdcbec4828170518a4604f2a650e6a7
MuhammedMagdyy <mohamedmody04@gmail.com>
2024-12-22 22:01:55 +0200

    feat: add main router with health check and route handling for projects, tags, tasks, and statuses

---
+++ src/routes/index.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { ApiError } from '../utils';
+import { NOT_FOUND, OK } from '../shared';
+import { projectRouter } from './projects';
+import { tagRouter } from './tags';
+import { taskRouter } from './tasks';
+import { statusRouter } from './statuses';
+
+const router = Router();
+
+router.get('/health', (_, res) => {
+  res.status(OK).json({ message: `I'm healthy 🤸‍♂️` });
+});
+
+router.use('/projects', projectRouter);
+router.use('/tags', tagRouter);
+router.use('/tasks', taskRouter);
+router.use('/statuses', statusRouter);
+
+router.all('*', (request, _res, next) => {
+  return next(new ApiError(`The route ${request.originalUrl} can't be found`, NOT_FOUND));
+});
+
+export default router;
