1bbb6bcc964e59addf495704c50e2a332a035fd5
MuhammedMagdyy <mohamedmody04@gmail.com>
2024-12-24 01:51:44 +0200

    refactor: remove deprecated status columns and implement status_uuid for projects, tasks, and statuses

--- src/database/prisma/migrations/20241223234956_models_and_relations/migration.sql
+++ src/database/prisma/migrations/20241223234956_models_and_relations/migration.sql
@@ -4,10 +4,10 @@ CREATE TABLE `projects` (
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
     `due_date` DATE NULL,
-    `status` ENUM('IN_PROGRESS', 'DONE', 'ARCHIVED', 'CANCELLED') NOT NULL DEFAULT 'IN_PROGRESS',
     `color` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `status_uuid` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`uuid`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -22,6 +22,7 @@ CREATE TABLE `tasks` (
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `project_uuid` VARCHAR(191) NOT NULL,
     `tag_uuid` VARCHAR(191) NULL,
+    `status_uuid` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`uuid`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -29,7 +30,18 @@ CREATE TABLE `tasks` (
 -- CreateTable
 CREATE TABLE `tags` (
     `uuid` VARCHAR(191) NOT NULL,
-    `title` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `color` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`uuid`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `statuses` (
+    `uuid` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
     `color` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -38,7 +50,13 @@ CREATE TABLE `tags` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
+ALTER TABLE `projects` ADD CONSTRAINT `projects_status_uuid_fkey` FOREIGN KEY (`status_uuid`) REFERENCES `statuses`(`uuid`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `tasks` ADD CONSTRAINT `tasks_project_uuid_fkey` FOREIGN KEY (`project_uuid`) REFERENCES `projects`(`uuid`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `tasks` ADD CONSTRAINT `tasks_tag_uuid_fkey` FOREIGN KEY (`tag_uuid`) REFERENCES `tags`(`uuid`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `tasks` ADD CONSTRAINT `tasks_status_uuid_fkey` FOREIGN KEY (`status_uuid`) REFERENCES `statuses`(`uuid`) ON DELETE RESTRICT ON UPDATE CASCADE;
