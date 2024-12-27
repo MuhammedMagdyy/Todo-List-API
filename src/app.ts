71a4d3e03a65b4c991fefb55104932701bcb954a
MuhammedMagdyy <mohamedmody04@gmail.com>
2024-12-27 22:43:03 +0200

    feat: integrate Swagger UI for API documentation and add necessary dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 import routes from './routes';
 import { errorHandler } from './middlewares';
 
@@ -18,6 +20,7 @@ const logger =
 app.use(logger);
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', routes);
 app.use(errorHandler);
 
