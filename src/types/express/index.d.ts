b85c779985a14d6be346c6be4841926c67edf5ee
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-06 01:21:38 +0200

    refactor: simplify Express Request and User interfaces by removing unused properties

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,17 +1,5 @@
 declare namespace Express {
   interface Request {
-    user?: {
-      uuid: string;
-    };
-    authInfo?: {
-      accessToken: string;
-      refreshToken: string;
-    };
-  }
-
-  interface User {
-    uuid: string;
-    providerId?: string;
-    tokens?: { accessToken: string; refreshToken: string };
+    user?: { uuid: string };
   }
 }
