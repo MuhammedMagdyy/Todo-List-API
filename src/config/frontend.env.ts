6dcc19f3967bdf947824d160b7798783fa1cc3ff
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-06 01:28:18 +0200

    refactor: update environment configuration and remove unused session variables

--- src/config/frontend.env.ts
+++ src/config/frontend.env.ts
@@ -1,5 +1,3 @@
 import env from './env';
 
 export const frontendUrl = env('FRONTEND_URL');
-export const frontendSuccessRedirectUrl = env('FRONTEND_SUCCESS_REDIRECT_URL');
-export const frontendErrorRedirectUrl = env('FRONTEND_FAILURE_REDIRECT_URL');
