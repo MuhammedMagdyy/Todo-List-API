ee24d3c2a55a3a631839002f537d99d25e2b7147
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-05 02:50:22 +0200

    refactor: add JWT type definition and expand HTTP exception status codes

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,5 +1,17 @@
 declare namespace Express {
+  interface Request {
+    user?: {
+      uuid: string;
+    };
+    authInfo?: {
+      accessToken: string;
+      refreshToken: string;
+    };
+  }
+
   interface User {
+    uuid: string;
     providerId?: string;
+    tokens?: { accessToken: string; refreshToken: string };
   }
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './query';
 export * from './status-codes';
+export * from './jwt';
